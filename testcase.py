2298d805011391252c7c6ff63b40a418ad3ea45e
phucphuong <phucphuong1293@gmail.com>
2017-11-20 23:49:46 +0700

    for kenh14.vn

--- testcase.py
+++ testcase.py
@@ -45,8 +45,8 @@ class ResponseURL():
                                 addXMLRequestCallback(function (xhr){ \
                                     setTimeout(function(){console.info(xhr.responseURL)}, 5000);}) \
                                 ')
-        self.browser.implicitly_wait(30)
-        # time.sleep(5)
+        # self.browser.implicitly_wait(30)
+        time.sleep(5)
         logs = self.browser.get_log('browser')
         # print(logs)
         responseURL = []
@@ -58,13 +58,14 @@ class ResponseURL():
         return responseURL
 
     def getResponseURLMultiplePage(self):
-        self.browser.implicitly_wait(30)
+        self.browser.implicitly_wait(10)
         self.responseURLs.append(self.getResponseURL())
-        self.browser.implicitly_wait(30)
+        self.browser.implicitly_wait(10)
         self.responseURLs.append(self.getResponseURL())
-        self.browser.implicitly_wait(30)
+        self.browser.implicitly_wait(10)
         self.responseURLs.append(self.getResponseURL())
-        self.browser.implicitly_wait(30)
+        self.browser.implicitly_wait(10)
+        time.sleep(5)
         self.browser.quit()
         self.responseURLs = sum(self.responseURLs, [])
         self.responseURLs = {self.url: list(set(self.responseURLs))}
