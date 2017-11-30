e039d51b4d53c5fe8d251d69f5a866d5036f19cd
Lance Lot <phucphuong1293@gmail.com>
2017-11-30 16:02:08 +0700

    fail version for soha

--- core.py
+++ core.py
@@ -1,39 +1,87 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.keys import Keys
 import time
-desired = DesiredCapabilities.CHROME
-desired['loggingPrefs'] = {'browser': 'INFO'}
-browser = webdriver.Chrome(desired_capabilities=desired)
-browser.get('http://cafef.vn/hang-hoa-nguyen-lieu/vat-lieu-xay-dung.chn')
-# time.sleep(10)
-browser.implicitly_wait(10)
-# browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-browser.execute_script('$( document ).ajaxSend(function(event, jqxhr, settings) { \
-                          console.info(settings.url) \
-                        });')
-browser.find_element_by_xpath("/html/body").send_keys(Keys.END)
+from selenium.webdriver.chrome.options import Options
+import threading
+import json
+import re
 
-# time.sleep(3)
-# browser.execute_script('function addXMLRequestCallback(callback){ \
-#                                     var oldSend, i; \
-#                                     if( XMLHttpRequest.callbacks ) { \
-#                                         XMLHttpRequest.callbacks.push( callback ); \
-#                                     } else { \
-#                                         XMLHttpRequest.callbacks = [callback]; \
-#                                         oldSend = XMLHttpRequest.prototype.send; \
-#                                         XMLHttpRequest.prototype.send = function(){ \
-#                                             for( i = 0; i < XMLHttpRequest.callbacks.length; i++ ){ \
-#                                                 XMLHttpRequest.callbacks[i]( this ); \
-#                                             } \
-#                                             oldSend.apply(this, arguments); \
-#                                         } \
-#                                     } \
-#                                 } \
-#                                 addXMLRequestCallback(function (xhr){ \
-#                                     setTimeout(function(){console.info(xhr.responseURL)}, 2000) \
-#                                 }) \
-#                                 ')
+
+url = 'http://soha.vn/video.htm'
+domain = re.search(r'(http|https):\/\/(.*?)/', url).group(0)[:-1]
+desired = DesiredCapabilities.CHROME
+desired['loggingPrefs'] = {'browser': 'ALL'}
+chrome_options = Options()
+prefs = {"profile.managed_default_content_settings.images": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument('no-sandbox')
+browser = webdriver.Chrome(chrome_options=chrome_options, desired_capabilities=desired)
+browser.implicitly_wait(30)
+browser.get(url)
+responseURLs = []
+
+browser.execute_script('$( document ).ajaxSend(function(event, jqxhr, settings) { \
+                                  console.info(settings.url) \
+                                });')
+
+# menu = browser.find_element_by_class_name('video-menu')
+
+
+
+# xpath = '//*[@id="video-paging"]/a[2]'
+# page = browser.find_element_by_xpath(xpath)
+# actions1 = ActionChains(browser)
+# actions1.move_to_element_with_offset(page, 0, 0)
+# actions1.click(page)
+# actions1.perform()
+
+
+links = browser.find_elements_by_xpath('//*[@id="admWrapsite"]/div[3]/div/div[3]/div[1]/ul/li')
+for link in links:
+    actions = ActionChains(browser)
+    actions.move_to_element_with_offset(link, 0, 0)
+    actions.click(link)
+    actions.perform()
+
+    time.sleep(3)
+
+    for i in range(3):
+        # xpath = '//*[@id="video-paging"]/a[{}]'.format(i)
+        # page = browser.find_element_by_xpath(xpath)
+        # print(page)
+        #
+        button = browser.find_element_by_id('btnViewMore')
+        actions1 = ActionChains(browser)
+        actions1.move_to_element_with_offset(button, 0, 0)
+        actions1.click(button)
+        actions1.perform()
+        time.sleep(3)
+
 time.sleep(5)
+
+
 logs = browser.get_log('browser')
-print(logs)
+domain = re.search(r'(http|https):\/\/(.*?)/', url).group(0)[:-1]
+test = []
+indicators = ["laytinmoitronglist", "trang-", "loadListNews", "page-", "page=", "Page", "loadListByPage",
+              "LoadListTinTrongNgay", "pageindex", "GetNewestVideoByZone", "video/latest/"]
+for el in logs:
+    if any(x in el["message"] for x in indicators):
+        # url = domain + el["message"].split("\"")[1]
+        test.append(el["message"])
+print(list(set(test)))
+
+# for t in ['vmenu_0', 'vmenu_49', 'vmenu_112', 'vmenu_110', 'vmenu_111']:
+#     button = browser.find_element_by_id(t)
+#     # button.click()
+#     # print(button.text)
+#     actions = ActionChains(browser)
+#     actions.move_to_element_with_offset(button, 3, 0)
+#     actions.click(button)
+#     actions.perform()
+#     time.sleep(5)
+browser.quit()
+    
+    
--- testcase.py
+++ testcase.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 from selenium.webdriver.chrome.options import Options
@@ -15,8 +16,8 @@ class ResponseURL():
         self.chrome_options = Options()
         self.prefs = {"profile.managed_default_content_settings.images": 2}
         self.chrome_options.add_experimental_option("prefs", self.prefs)
-        self.chrome_options.add_argument("--headless")
-        self.chrome_options.add_argument('no-sandbox')
+        # self.chrome_options.add_argument("--headless")
+        # self.chrome_options.add_argument('no-sandbox')
         self.browser = webdriver.Chrome(chrome_options=self.chrome_options, desired_capabilities=self.desired)
         self.browser.implicitly_wait(30)
         self.browser.get(url)
@@ -26,22 +27,40 @@ class ResponseURL():
         self.browser.execute_script('$( document ).ajaxSend(function(event, jqxhr, settings) { \
                                   console.info(settings.url) \
                                 });')
-        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(6)
+        # '''change action here'''
+        # time.sleep(4)
+        # # button = self.browser.find_element_by_class_name('buttonxtttn')
+        # print(url)
+        try:
+        #     # button = self.browser.find_element_by_id(id)
+        #     actions1 = ActionChains(self.browser)
+        #     actions1.move_to_element_with_offset(url, 0, 0)
+        #     actions1.click(url)
+        #     actions1.perform()
+        #     # self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # # self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            button = self.browser.find_element_by_id('btnViewMore')
+            actions1 = ActionChains(self.browser)
+            actions1.move_to_element_with_offset(button, 0, 0)
+            actions1.click(button)
+            actions1.perform()
+            # time.sleep(3)
+        except:
+            pass
     def getResponseURLMultiplePage(self, times):
         for t in range(times):
             self.getResponseURL()
-        logs = self.browser.get_log('browser')
-        responseURL = []
-        indicators = ["laytinmoitronglist", "trang-", "loadListNews", "page-", "page=", "Page"]
-        for el in logs:
-            if any(x in el["message"] for x in indicators):
-                url = self.domain + el["message"].split("\"")[1]
-                responseURL.append(url)
-
-        self.responseURLs = {self.url: list(set(responseURL))}
+        # logs = self.browser.get_log('browser')
+        # responseURL = []
+        # indicators = ["laytinmoitronglist", "trang-", "loadListNews", "page-", "page=", "Page", "loadListByPage", "LoadListTinTrongNgay", "pageindex"]
+        # for el in logs:
+        #     if any(x in el["message"] for x in indicators):
+        #         url = self.domain + el["message"].split("\"")[1]
+        #         responseURL.append(url)
+        #
+        # self.responseURLs = {self.url: list(set(responseURL))}
         # time.sleep(5)
-        self.browser.quit()
+        # self.browser.quit()
 
 class Worker(threading.Thread):
     def __init__(self, threadID, listCategory):
@@ -51,11 +70,41 @@ class Worker(threading.Thread):
         self.listResponseURl = []
     def run(self):
         for el in self.listCategory:
-            print(el)
             responseURL = ResponseURL(el)
-            responseURL.getResponseURLMultiplePage(3)
-            self.listResponseURl.append(responseURL.responseURLs)
-
+            print(el)
+            # menu = responseURL.browser.find_element_by_class_name('video-menu')
+            # links = menu.find_elements_by_xpath('.//ul/li')
+            # for t in links:
+            #     # button = responseURL.browser.find_element_by_id(t)
+            #     # button.click()
+            #     # button = temp.find_element_by_xpath(".//a")
+            #     # print(button.text)
+            #     actions = ActionChains(responseURL.browser)
+            #     actions.move_to_element_with_offset(t, 0, 0)
+            #     actions.click(t)
+            #     actions.perform()
+            #     responseURL.browser.implicitly_wait(6)
+            #     time.sleep(6)
+            #
+            #     # list_url = ["aCategory_1", "aCategory_2", "aCategory_3", "aCategory_4"]
+            #     list_url = []
+            #     for i in range(1, 5):
+            #         xpath = '//*[@id="video-paging"]/a[{}]'.format(i)
+            #         list_url.append(responseURL.browser.find_element_by_xpath(xpath))
+            responseURL.getResponseURLMultiplePage(4)
+                # print(responseURL.responseURLs)
+                # responseURL.responseURLs = []
+                # self.listResponseURl.append(responseURL.responseURLs)
+        logs = responseURL.browser.get_log('browser')
+        test = []
+        indicators = ["laytinmoitronglist", "trang-", "loadListNews", "page-", "page=", "Page", "loadListByPage",
+                      "LoadListTinTrongNgay", "pageindex", "GetNewestVideoByZone", "video/latest/"]
+        for el in logs:
+            if any(x in el["message"] for x in indicators):
+                # url = responseURL.domain + el["message"].split("\"")[1]
+                test.append(el["message"])
+        print(list(set(test)))
+            # self.responseURLs = {self.url: list(set(responseURL))}
 def getCategory(file_path):
     file = open(file_path, 'r')
     content = file.readlines()
@@ -63,7 +112,7 @@ def getCategory(file_path):
     return content
 
 def scheduleThread(numThreads):
-    category = getCategory('category.txt')
+    category = getCategory('nld.txt')
     numElements = len(category) // numThreads
     listThreads = []
     data = []
@@ -83,7 +132,7 @@ def scheduleThread(numThreads):
     return data
 
 if __name__ == '__main__':
-    data = scheduleThread(4)
+    data = scheduleThread(1)
     data = sum(data, [])
     data = json.dumps(data)
     with open('temp.json', 'w') as outfile:
